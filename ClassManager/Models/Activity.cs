bbcb3e321149f408a9868435e8c5d844fad48e69
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-07-19 12:13:48 +0800

    add Models - Activity and Person

---
+++ ClassManager/Models/Activity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassManager.Models
+{
+    /// <summary>
+    /// 活动类
+    /// </summary>
+    public class Activity
+    {
+        public string Model { get; set; }       // Django 对应 Model
+        public int PrimaryKey { get; set; }     // 数据库主键
+        public Fields Info { get; set; }        // 详细信息
+
+        public class Fields
+        {
+            public string Name { get; set; }            // 活动名
+            public string Date { get; set; }            // 开始日期
+            public string Time { get; set; }            // 开始时间
+            public string Place { get; set; }           // 活动地点
+            public string Content { get; set; }         // 活动内容
+            public int Participation { get; set; }      // 参与得分
+            public string Participator { get; set; }    // 参与人员（学号以半角逗号分隔）
+            public string ImagesUrl { get; set; }       // 相关图片url（以半角逗号分隔）
+        }
+    }
+}
---
+++ ClassManager/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassManager.Models
+{
+    /// <summary>
+    /// 学生类
+    /// </summary>
+    public class Person
+    {
+        public string Model { get; set; }       // Django 对应 Model
+        public int PrimaryKey { get; set; }     // 数据库主键
+        public Fields Info { get; set; }        // 详细信息
+
+        public class Fields
+        {
+            public string StudentNumber { get; set; }       // 学号
+            public string Name { get; set; }                // 姓名
+            public string Pinyin { get; set; }              // 拼音
+            public string Gender { get; set; }              // 性别（F/M）
+            public string NativeProvince { get; set; }      // 籍贯
+            public string Dormitory { get; set; }           // 寝室号
+            public string Birthday { get; set; }            // 生日（公历）
+            public string PhoneNumber { get; set; }         // 手机号
+            public string Position { get; set; }            // 职务
+            public int Participation { get; set; }          // 活动参与分
+        }
+    }
+}
