d6d409a78fb0160c819c5995d759524f92ea1b77
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-07-19 23:01:56 +0800

    Make Models more simple

--- ClassManager/Models/Activity.cs
+++ ClassManager/Models/Activity.cs
@@ -11,20 +11,14 @@ namespace ClassManager.Models
     /// </summary>
     public class Activity
     {
-        public string Model { get; set; }       // Django 对应 Model
-        public int PrimaryKey { get; set; }     // 数据库主键
-        public Fields Info { get; set; }        // 详细信息
-
-        public class Fields
-        {
-            public string Name { get; set; }            // 活动名
-            public string Date { get; set; }            // 开始日期
-            public string Time { get; set; }            // 开始时间
-            public string Place { get; set; }           // 活动地点
-            public string Content { get; set; }         // 活动内容
-            public int Participation { get; set; }      // 参与得分
-            public string Participator { get; set; }    // 参与人员（学号以半角逗号分隔）
-            public string ImagesUrl { get; set; }       // 相关图片url（以半角逗号分隔）
-        }
+        public int Id { get; set; }                 // 数据库主键
+        public string Name { get; set; }            // 活动名
+        public string Date { get; set; }            // 开始日期
+        public string Time { get; set; }            // 开始时间
+        public string Place { get; set; }           // 活动地点
+        public string Content { get; set; }         // 活动内容
+        public int Participation { get; set; }      // 参与得分
+        public string Participator { get; set; }    // 参与人员（学号以半角逗号分隔）
+        public string ImagesUrl { get; set; }       // 相关图片url（以半角逗号分隔）
     }
 }
--- ClassManager/Models/Person.cs
+++ ClassManager/Models/Person.cs
@@ -11,22 +11,16 @@ namespace ClassManager.Models
     /// </summary>
     public class Person
     {
-        public string Model { get; set; }       // Django 对应 Model
-        public int PrimaryKey { get; set; }     // 数据库主键
-        public Fields Info { get; set; }        // 详细信息
-
-        public class Fields
-        {
-            public string StudentNumber { get; set; }       // 学号
-            public string Name { get; set; }                // 姓名
-            public string Pinyin { get; set; }              // 拼音
-            public string Gender { get; set; }              // 性别（F/M）
-            public string NativeProvince { get; set; }      // 籍贯
-            public string Dormitory { get; set; }           // 寝室号
-            public string Birthday { get; set; }            // 生日（公历）
-            public string PhoneNumber { get; set; }         // 手机号
-            public string Position { get; set; }            // 职务
-            public int Participation { get; set; }          // 活动参与分
-        }
+        public string Id { get; set; }                  // 数据库主键
+        public string StudentNumber { get; set; }       // 学号
+        public string Name { get; set; }                // 姓名
+        public string Pinyin { get; set; }              // 拼音
+        public string Gender { get; set; }              // 性别（F/M）
+        public string NativeProvince { get; set; }      // 籍贯
+        public string Dormitory { get; set; }           // 寝室号
+        public string Birthday { get; set; }            // 生日（公历）
+        public string PhoneNumber { get; set; }         // 手机号
+        public string Position { get; set; }            // 职务
+        public int Participation { get; set; }          // 活动参与分
     }
 }
