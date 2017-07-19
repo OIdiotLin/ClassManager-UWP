2c9176fd84a4162137634a3d4c4f4a9b0f99c144
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-07-20 00:11:19 +0800

    classify URLs

--- ClassManager/Networks/APIUrl.cs
+++ ClassManager/Networks/APIUrl.cs
@@ -13,36 +13,45 @@ namespace ClassManager.Networks
     {
         // Prefix - protocol and common path
         public static string prefix = "http://server.oidiotlin.com/clsmngr/api/";
-        
-        // GET - 获取学生列表
-        public static string GetPersonList = prefix + "get_person_list/";
 
-        // POST - 添加学生
-        public static string AddPerson = prefix + "add_person/";
+        public static class Person
+        {
+            // GET - 获取学生列表
+            public static string GetPersonList = prefix + "get_person_list/";
 
-        // POST - 删除学生
-        public static string DeletePerson = prefix + "delete_person/";
+            // POST - 添加学生
+            public static string AddPerson = prefix + "add_person/";
 
-        // POST - 修改学生信息
-        public static string UpdatePerson = prefix + "update_person/";
+            // POST - 删除学生
+            public static string DeletePerson = prefix + "delete_person/";
 
-        // GET - 按活动编号查找学生
-        public static string GetPersonByActivity = prefix + "get_person_by_activity/";
+            // POST - 修改学生信息
+            public static string UpdatePerson = prefix + "update_person/";
 
-        // GET - 获取活动列表
-        public static string GetActivityList = prefix + "get_activity_list/";
+            // GET - 按活动编号查找学生
+            public static string GetPersonByActivity = prefix + "get_person_by_activity/";
+        }
 
-        // POST - 添加活动
-        public static string AddActivity = prefix + "add_activity/";
+        public static class Activity
+        {
+            // GET - 获取活动列表
+            public static string GetActivityList = prefix + "get_activity_list/";
 
-        // POST - 删除活动
-        public static string DeleteActivity = prefix + "delete_activity/";
+            // POST - 添加活动
+            public static string AddActivity = prefix + "add_activity/";
 
-        // POST - 修改活动信息
-        public static string UpdateActivity = prefix + "update_activity/";
+            // POST - 删除活动
+            public static string DeleteActivity = prefix + "delete_activity/";
 
-        // GET - 获取七牛云上传凭证
-        public static string GetUploadToken = prefix + "get_upload_token/";
+            // POST - 修改活动信息
+            public static string UpdateActivity = prefix + "update_activity/";
+        }
+
+        public static class Qiniu
+        {
+            // GET - 获取七牛云上传凭证
+            public static string GetUploadToken = prefix + "get_upload_token/";
+        }
 
     }
 }
