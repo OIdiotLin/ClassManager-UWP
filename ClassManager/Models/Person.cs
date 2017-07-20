585c935d30221cc7b838fe291b59bb1bbc200fc6
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-07-20 23:59:55 +0800

    add method ToJson()

--- ClassManager/Models/Person.cs
+++ ClassManager/Models/Person.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ClassManager.Networks;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Data.Json;
 
 namespace ClassManager.Models
 {
@@ -22,5 +25,37 @@ namespace ClassManager.Models
         public string PhoneNumber { get; set; }         // 手机号
         public string Position { get; set; }            // 职务
         public int Participation { get; set; }          // 活动参与分
+
+        public string ToJson()
+        {
+            try
+            {
+                JObject subjson = new JObject
+                {
+                    { APIKey.Person.Id, this.Id },
+                    { APIKey.Person.Name, this.Name },
+                    { APIKey.Person.StudentNumber,this.StudentNumber },
+                    { APIKey.Person.Pinyin, this.Pinyin },
+                    { APIKey.Person.Gender, this.Gender },
+                    { APIKey.Person.NativeProvince, this.NativeProvince },
+                    { APIKey.Person.Dormitory, this.Dormitory },
+                    { APIKey.Person.PhoneNumber, this.PhoneNumber },
+                    { APIKey.Person.Birthday, this.Birthday },
+                    { APIKey.Person.Position, this.Position },
+                    { APIKey.Person.Participation, this.Participation }
+                };
+
+                JObject json = new JObject
+                {
+                    { APIKey.Person.Root, subjson }
+                };
+
+                return json.ToString();
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
