12c9525d656124667f8a9f113432d317958d5d90
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-07-29 21:06:44 +0800

    Use INotifyPropertyChanged to handle event - property changed

---
+++ ClassManager/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassManager.ViewModels
+{
+    [DataContract]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
