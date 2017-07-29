434110e4f88df5e34b834c87f451e10f21099534
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-07-29 21:28:14 +0800

    MVVM - VM - LoginViewModel login

---
+++ ClassManager/ViewModels/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ClassManager.Networks;
+using ClassManager.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassManager.ViewModels
+{
+    public class LoginViewModel: BaseViewModel
+    {
+        APIService api = new APIService();
+
+        /// <summary>
+        /// 是否登录成功
+        /// </summary>
+        private bool _is_login_success;
+        public bool IsLoginSuccess {
+            get {
+                return _is_login_success;
+            }
+            set {
+                _is_login_success = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 以管理员身份登录
+        /// </summary>
+        /// <param name="password">密码</param>
+        private async void Login(string password)
+        {
+            IsLoginSuccess = await api.Login(password, Rand.RandomString());
+        }
+    }
+}
