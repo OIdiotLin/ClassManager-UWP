4935051f716b1f96f6b5798d038f9cc49500f5c9
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-08-06 22:47:10 +0800

    ActivityPage Init

---
+++ ClassManager/ViewModels/ActivityDetailsViewModel.cs
@@ -0,0 +1,56 @@
+﻿using ClassManager.Models;
+using ClassManager.Networks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassManager.ViewModels
+{
+    public class ActivityDetailsViewModel: BaseViewModel
+    {
+        APIService api = new APIService();
+
+        /// <summary>
+        /// 该页面作为展示的<see cref="Activity"/>
+        /// </summary>
+        private Activity _activity_on_display;
+        public Activity ActivityOnDisplay {
+            get {
+                return this._activity_on_display;
+            }
+            set {
+                this._activity_on_display = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 提交POST表单，删除<see cref="ActivityOnDisplay"/>
+        /// </summary>
+        /// <returns>删除结果</returns>
+        public async Task<bool> DeleteActivityAsync()
+        {
+            return await api.DeleteActivity(ActivityOnDisplay);
+        }
+
+        /// <summary>
+        /// 提交POST表单，添加<see cref="ActivityOnDisplay"/>
+        /// </summary>
+        /// <returns>添加结果</returns>
+        public async Task<bool> AddActivityAsync()
+        {
+            return await api.AddActivity(ActivityOnDisplay);
+        }
+
+        /// <summary>
+        /// 提交POST表单，修改<see cref="ActivityOnDisplay"/>
+        /// </summary>
+        /// <returns>修改结果</returns>
+        public async Task<bool> UpdateActivityAsync()
+        {
+            return await api.UpdateActivity(ActivityOnDisplay.Id, ActivityOnDisplay);
+        }
+    }
+}
---
+++ ClassManager/Views/ActivitySchedulePage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using ClassManager.ViewModels;
+using ClassManager.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using ClassManager.Controls;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace ClassManager.Views
+{
+    /// <summary>
+    /// <see cref="Activity"/>的一览页面
+    /// </summary>
+    public sealed partial class ActivitySchedulePage : Page
+    {
+        private ActivityScheduleViewModel vm;
+
+        public ActivitySchedulePage()
+        {
+            this.InitializeComponent();
+            vm = new ActivityScheduleViewModel();
+        }
+
+        /// <summary>
+        /// 导航至<see cref="ActivityPage"/>时所触发的函数
+        /// </summary>
+        /// <param name="e"></param>
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            LoadingProgressRing.IsActive = true;
+            await vm.GetActivities();
+            LoadingProgressRing.IsActive = false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ActivityGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var Root = ((Parent as Frame).Parent as Grid).Parent as ActivityPage;
+            var sourceActivity = e.ClickedItem as Activity;
+            Root.NavigateFrame(typeof(ActivityDetailsPage), sourceActivity);
+        }
+    }
+
+}
