eec2b79941210393d158a47a3514eb1f63a2a8a1
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-08-06 09:05:43 +0800

    Activity V-VM Initialized

---
+++ ClassManager/ViewModels/ActivityViewModel.cs
@@ -0,0 +1,45 @@
+﻿using ClassManager.Models;
+using ClassManager.Networks;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassManager.ViewModels
+{
+    public class ActivityViewModel: BaseViewModel
+    {
+        APIService api = new APIService();
+
+        /// <summary>
+        /// 活动
+        /// </summary>
+        private ObservableCollection<Activity> _activities;
+        public ObservableCollection<Activity> Activities {
+            get {
+                return this._activities;
+            }
+            set {
+                this._activities = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 请求并更新<seealso cref="Activities"/>
+        /// </summary>
+        /// <returns></returns>
+        public async Task GetActivities()
+        {
+            var list = await api.GetActivityList();
+            Activities = new ObservableCollection<Activity>();
+            foreach(var item in list)
+            {
+                Activities.Add(item);
+            }
+        }
+
+    }
+}
---
+++ ClassManager/Views/ActivityPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ClassManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace ClassManager.Views
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class ActivityPage : Page
+    {
+        private ActivityViewModel vm;
+
+        public ActivityPage()
+        {
+            this.InitializeComponent();
+            vm = new ActivityViewModel();
+        }
+
+        /// <summary>
+        /// 导航至<see cref="ActivityPage"/>时所触发的函数
+        /// </summary>
+        /// <param name="e"></param>
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await vm.GetActivities();
+        }
+    }
+}
