327d3dc87443613efce6be0a9fb7bd69260aebb3
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-08-08 21:08:15 +0800

    Buttons Function Finished

--- ClassManager/Views/ActivityPage.xaml.cs
+++ ClassManager/Views/ActivityPage.xaml.cs
@@ -21,7 +21,9 @@ using Windows.UI.Xaml.Navigation;
 namespace ClassManager.Views
 {
     /// <summary>
-    /// <see cref="ActivityPage"/>的主页面，其中的<see cref="ActivityMainFrame"/>是对<see cref="Activity"/>的UI重心
+    /// <see cref="ActivityPage"/>的主页面，其中的<see cref="ActivityMainFrame"/>可包含
+    /// <see cref="ActivityDetailsPage"/>、<see cref="ActivitySchedulePage"/>、
+    /// <see cref="ActivityEditingPage"/>。
     /// </summary>
     public sealed partial class ActivityPage : Page
     {
@@ -37,12 +39,32 @@ namespace ClassManager.Views
             }
             set {
                 _frame_page_type = value;
+
+                GoBackButton.Visibility = (_frame_page_type == typeof(ActivitySchedulePage) ? Visibility.Collapsed : Visibility.Visible);
+
                 if (App.IsAdmin)
                 {
-                    GoBackButton.Visibility = (_frame_page_type == typeof(ActivityDetailsPage) ? Visibility.Visible : Visibility.Collapsed);
-                    AddButton.Visibility = (_frame_page_type == typeof(ActivitySchedulePage) ? Visibility.Visible : Visibility.Collapsed);
-                    DeleteButton.Visibility = (_frame_page_type == typeof(ActivityDetailsPage) ? Visibility.Visible : Visibility.Collapsed);
-                    UpdateButton.Visibility = (_frame_page_type == typeof(ActivityDetailsPage) ? Visibility.Visible : Visibility.Collapsed);
+                    if(_frame_page_type == typeof(ActivitySchedulePage))
+                    {
+                        AddButton.Visibility = Visibility.Visible;
+                        ConfirmButton.Visibility = Visibility.Collapsed;
+                        DeleteButton.Visibility = Visibility.Collapsed;
+                        UpdateButton.Visibility = Visibility.Collapsed;
+                    }
+                    else if(_frame_page_type == typeof(ActivityDetailsPage))
+                    {
+                        AddButton.Visibility = Visibility.Collapsed;
+                        ConfirmButton.Visibility = Visibility.Collapsed;
+                        DeleteButton.Visibility = Visibility.Visible;
+                        UpdateButton.Visibility = Visibility.Visible;
+                    }
+                    else if(_frame_page_type == typeof(ActivityEditingPage))
+                    {
+                        AddButton.Visibility = Visibility.Collapsed;
+                        ConfirmButton.Visibility = Visibility.Visible;
+                        DeleteButton.Visibility = Visibility.Collapsed;
+                        UpdateButton.Visibility = Visibility.Collapsed;
+                    }
                 }
             }
         }
@@ -52,40 +74,7 @@ namespace ClassManager.Views
             base.OnNavigatedTo(e);
             FramePageType = typeof(ActivitySchedulePage);
         }
-
-        ///// <summary>
-        ///// 由<see cref="ActivityMainFrame"/>的内容物决定<see cref="AddButton"/>的<see cref="Visibility"/>：
-        ///// 当内容为<see cref="ActivitySchedulePage"/>时，<see cref="AddButton"/>为可见，
-        ///// 当内容为<see cref="ActivityDetailsPage"/>时，<see cref="AddButton"/>为不可见。
-        ///// </summary>
-        //private Visibility AddButtonVisibility {
-        //    get {
-        //        return ActivityMainFrame.SourcePageType == typeof(ActivitySchedulePage) && App.IsAdmin ? Visibility.Visible : Visibility.Collapsed;
-        //    }
-        //}
-
-        ///// <summary>
-        ///// 由<see cref="ActivityMainFrame"/>的内容物决定<see cref="DeleteButton"/>的<see cref="Visibility"/>：
-        ///// 当内容为<see cref="ActivitySchedulePage"/>时，<see cref="DeleteButton"/>为不可见，
-        ///// 当内容为<see cref="ActivityDetailsPage"/>时，<see cref="DeleteButton"/>为可见。
-        ///// </summary>
-        //private Visibility DeleteButtonVisibility {
-        //    get {
-        //        return ActivityMainFrame.SourcePageType == typeof(ActivityDetailsPage) && App.IsAdmin ? Visibility.Visible : Visibility.Collapsed;
-        //    }
-        //}
-
-        ///// <summary>
-        ///// 由<see cref="ActivityMainFrame"/>的内容物决定<see cref="UpdateButton"/>的<see cref="Visibility"/>：
-        ///// 当内容为<see cref="ActivitySchedulePage"/>时，<see cref="UpdateButton"/>为不可见，
-        ///// 当内容为<see cref="ActivityDetailsPage"/>时，<see cref="UpdateButton"/>为可见。
-        ///// </summary>
-        //private Visibility UpdateButtonVisibility {
-        //    get {
-        //        return ActivityMainFrame.SourcePageType == typeof(ActivityDetailsPage) && App.IsAdmin ? Visibility.Visible : Visibility.Collapsed;
-        //    }
-        //}
-
+        
         /// <summary>
         /// <see cref="ActivityMainFrame"/>跳转到<paramref name="sourcePageType"/>，并展示<paramref name="sourceActivity"/>
         /// </summary>
@@ -109,14 +98,15 @@ namespace ClassManager.Views
         }
 
         /// <summary>
-        /// <see cref="AddButton"/>被点击，打开<see cref="ActivityDetailsPage"/>，并传入新<see cref="Activity"/>
+        /// <see cref="AddButton"/>被点击，切换至<see cref="ActivityEditingPage"/>，并传入新<see cref="Activity"/>
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            this.ActivityMainFrame.Navigate(typeof(ActivityDetailsPage), new Activity());
-            FramePageType = typeof(ActivityDetailsPage);
+            this.ActivityMainFrame.Navigate(typeof(ActivityEditingPage), new Activity());
+            FramePageType = typeof(ActivityEditingPage);
+            ConfirmButton.Tag = "Add";
         }
 
         /// <summary>
@@ -130,13 +120,43 @@ namespace ClassManager.Views
         }
 
         /// <summary>
-        /// <see cref="UpdateButton"/>被点击，调用<see cref="ActivityDetailsPage.UpdateActivity"/>
+        /// <see cref="UpdateButton"/>被点击，切换至<see cref="ActivityEditingPage"/>
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void UpdateButton_Click(object sender, RoutedEventArgs e)
         {
-            (this.ActivityMainFrame.Content as ActivityDetailsPage).UpdateActivity();
+            this.ActivityMainFrame.Navigate(typeof(ActivityEditingPage),
+                (ActivityMainFrame.Content as ActivityDetailsPage).SourceActivity);
+            FramePageType = typeof(ActivityEditingPage);
+            ConfirmButton.Tag = "Update";
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            FramePageType = typeof(ActivitySchedulePage);
+        }
+
+        /// <summary>
+        /// <see cref="ConfirmButton"/>被点击，根据<see cref="ActivityMainFrame"/>内容物决定操作类型：
+        /// 若是添加操作则调用<see cref="ActivityDetailsPage.ConfirmAdd"/>，
+        /// 若是修改操作则调用<see cref="ActivityDetailsPage.ConfirmUpdate"/>。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch((sender as Button).Tag as string)
+            {
+                case "Add":
+                    //(this.ActivityMainFrame.Content as ActivityDetailsPage).ConfirmAdd();
+                    break;
+                case "Update":
+                    //(this.ActivityMainFrame.Content as ActivityDetailsPage).ConfirmUpdate();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
