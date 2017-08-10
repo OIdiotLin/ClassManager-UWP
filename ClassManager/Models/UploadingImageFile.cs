ceadfbac730808235aa30afaf6ac9f25c41e2939
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-08-10 17:36:38 +0800

    UploadingImageFile initialized.

---
+++ ClassManager/Models/UploadingImageFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace ClassManager.Models
+{
+    /// <summary>
+    /// 需要上传的Image类型
+    /// </summary>
+    public sealed class UploadingImageFile : Object
+    {
+        /// <summary>
+        /// 文件本体
+        /// </summary>
+        public StorageFile File { get; set; }
+
+        /// <summary>
+        /// 上传状态枚举类型
+        /// </summary>
+        public enum UPLOADSTATE
+        {
+            ReadyForUpload,
+            Uploading,
+            UploadSuccess,
+            UploadFails
+        }
+
+        /// <summary>
+        /// 上传状态
+        /// </summary>
+        public UPLOADSTATE UploadState { get; set; }
+    }
+}
