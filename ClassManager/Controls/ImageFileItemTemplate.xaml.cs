b37faf87456446112e0ebf7f2df03089946f153a
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-08-10 21:16:14 +0800

    ImageFileItem usercontrol finished

---
+++ ClassManager/Controls/ImageFileItemTemplate.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ClassManager.Controls
+{
+    public sealed partial class ImageFileItemTemplate : UserControl
+    {
+        public Models.UploadingImageFile UploadingImageFile {
+            get {
+                return this.DataContext as Models.UploadingImageFile;
+            }
+            set {
+                this.DataContext = value;
+                IconRefresh();
+            }
+        }
+
+        /// <summary>
+        /// 根据上传状态更新Icons
+        /// </summary>
+        private void IconRefresh()
+        {
+            switch (UploadingImageFile.State)
+            {
+                case Models.UploadingImageFile.UploadState.ReadyForUpload:  // 准备上传
+                    UploadFailIcon.Visibility = Visibility.Collapsed;
+                    UploadSuccessIcon.Visibility = Visibility.Collapsed;
+                    UploadingProgressRing.IsActive = false;
+                    break;
+                case Models.UploadingImageFile.UploadState.Uploading:       // 上传中
+                    UploadFailIcon.Visibility = Visibility.Collapsed;
+                    UploadSuccessIcon.Visibility = Visibility.Collapsed;
+                    UploadingProgressRing.IsActive = true;
+                    break;
+                case Models.UploadingImageFile.UploadState.UploadSuccess:   // 上传成功
+                    UploadFailIcon.Visibility = Visibility.Collapsed;
+                    UploadSuccessIcon.Visibility = Visibility.Visible;
+                    UploadingProgressRing.IsActive = false;
+                    break;
+                case Models.UploadingImageFile.UploadState.UploadFails:     // 上传失败
+                    UploadFailIcon.Visibility = Visibility.Visible;
+                    UploadSuccessIcon.Visibility = Visibility.Collapsed;
+                    UploadingProgressRing.IsActive = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public ImageFileItemTemplate()
+        {
+            this.InitializeComponent();
+
+            this.DataContextChanged += (s, e) => Bindings.Update();
+        }
+    }
+}
